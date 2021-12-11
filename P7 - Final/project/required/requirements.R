73d935a277c31056bfc81bd0a342dc689f3bb57a
achristopher4 <59030129+achristopher4@users.noreply.github.com>
2021-12-10 19:28:15 -0500

    Add files via upload

---
+++ P7 - Final/project/required/requirements.R
@@ -0,0 +1,4 @@
+install.packages("data.table")
+install.packages("httr")
+install.packages("Rtsne")
+install.packages("xgboost")
---
+++ P7 - Final/run_project.R
@@ -0,0 +1,9 @@
+# Run Main.R
+## Feature Selection and manipulation
+source('./project/src/features/Classification_Feature.R')
+source('./project/src/features/Embedded.R')
+source('./project/src/features/Feature.R')
+source('./project/src/features/tsne_feature.R')
+
+## Model
+source('./project/src/models/XGBoost_3_TNSE_Final.R')
